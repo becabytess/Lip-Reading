498c95813ca79e746693dddd074301c578eaef72
Beka <beka.alemuu@gmail.com>
2026-07-18 17:21:25 +0000

    data exploration complete

---
+++ download_dataset.py
@@ -0,0 +1,12 @@
+import kagglehub
+import shutil
+
+# Download latest version
+path = kagglehub.dataset_download("jedidiahangekouakou/grid-corpus-dataset-for-training-lipnet")
+
+print("Path to dataset files:", path)
+
+# Copy the dataset to a local directory
+shutil.move(path, "../dataset")
+
+
